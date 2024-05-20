2495f671df736f02ddeaed1d748e265f6f9d81bd
Gileno <gileno@tsmx.net.br>
2024-05-20 08:57:46 -0300

    testes in golang

---
+++ teste/soma.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := soma(1, 2, 3)
+
+	fmt.Println(x)
+}
+
+func soma(i ...int) int {
+	total := 0
+
+	for _, v := range i {
+		total += v
+	}
+	return total
+
+}
