56854af1d9ffff9aaca691e3b224a7d655f3d18b
Gileno <gileno@tsmx.net.br>
2024-05-21 08:46:23 -0300

    testes

--- teste/soma_test.go
+++ teste/soma_test.go
@@ -4,9 +4,19 @@ import (
 	"testing"
 )
 
-func TestSoma(t *testing.T) {
+func ShouldSumCorrect(t *testing.T) {
 	teste := soma(3, 2, 1)
 	resultado := 6
+
+	if teste != resultado {
+		t.Error("Valor esperado: ", resultado, "Valor retornado", teste)
+	}
+
+}
+
+func ShouldSumIncorrect(t *testing.T) {
+	teste := soma(3, 2, 1)
+	resultado := 7
 
 	if teste != resultado {
 		t.Error("Valor esperado: ", resultado, "Valor retornado", teste)
